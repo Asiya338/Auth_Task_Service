3f26ed9de8bd876e331d18c0ff67ad35cf18a14a
Asiya Shaik <shaik333asiya@gmail.com>
2026-02-04 20:15:48 +0530

    Initial project setup with dependencies and MySQL configuration

---
+++ src/main/java/com/primetrade/ai/authtask/AuthenticationTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.primetrade.ai.authtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationTaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationTaskServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primetrade/ai/authtask/AuthenticationTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primetrade.ai.authtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
